a9ab404134ff60752d3ba7d9077085e6e09acc51
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-08-18 10:44:28 -0400

    better test description
    [skip ci]

--- tests/testthat/test-sync.R
+++ tests/testthat/test-sync.R
@@ -1,4 +1,4 @@
-test_that("errors if no envvar available", {
+test_that("warns if no envvar available", {
   withr::with_envvar(c("DARKLY_LIGHT_THEME" = "", "DARKLY_DARK_THEME" = ""), {
     expect_warning(get_light_theme(), regexp = "DARKLY_LIGHT_THEME")
     expect_warning(get_dark_theme(), regexp = "DARKLY_DARK_THEME")
