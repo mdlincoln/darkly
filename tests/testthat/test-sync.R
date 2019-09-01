3d9959689b7df82fa3161d4f4fd8e1c99815dcca
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2019-09-01 16:20:15 -0400

    test that correct envvar is returned

--- tests/testthat/test-sync.R
+++ tests/testthat/test-sync.R
@@ -4,3 +4,12 @@ test_that("warns if no envvar available", {
     expect_warning(get_dark_theme(), regexp = "DARKLY_DARK_THEME")
   })
 })
+
+test_that("returns correct envvar", {
+  lt = "Texmate (default)"
+  dt = "Solarized Dark"
+  withr::with_envvar(c("DARKLY_LIGHT_THEME" = lt, "DARKLY_DARK_THEME" = dt), {
+    expect_equivalent(get_light_theme(), lt)
+    expect_equivalent(get_dark_theme(), dt)
+  })
+})
